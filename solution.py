43a02089bc65c70475594d03fa700f104f7e6da3
Alex Zhao <alexzhao21@gmail.com>
2018-11-30 23:19:31 -0800

    Remove class in favor of readability and clean up some code (add more comments, etc)

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import requests
 import sys
 
@@ -16,94 +16,93 @@ backtrack = {
     'RIGHT': 'LEFT'
 }
 
-class Challenge:
-    def __init__(self):
-        self.token_param = {}
-        self.game = {}
+token_param = {}
+game = {}
+
+# start the challenge
+def start_challenge():
+    global token_param
+
+    # get our token
+    form_data = {'uid': '904907409'}
+    token_param = requests.post(url + '/session', data=form_data).json()
+
+    print("Obtained token: {}".format(token_param['token']))
+
+    # begin playing games and NEVER STOP until we finish
+    while True:
+        if update_game_info(): # will also check for completeness
+            print("Completed all levels. Challenge complete!")
+            return
+
+        # set up 2D maze matrix (start w/ all False for unvisited)
+        maze = [[False for x in range(game['maze_size'][0])] for y in range(game['maze_size'][1])]
+        play_level(maze, game['current_location'][0], game['current_location'][1])
+
+        print("Level complete!")
+
+# returns True if challenge is finished, False if not
+# we only need to check this at the start of each new level
+def update_game_info():
+    global game
+
+    game = requests.get(url + '/game', params=token_param).json()
+
+    # check status
+    if game['status'] == 'NONE':
+        print("Status is NONE, i.e. session has expired or does not exist")
+        sys.exit(1)
+    elif game['status'] == 'GAME_OVER':
+        print("Status is GAME_OVER, i.e. we lost :(")
+        sys.exit(1)
+    elif game['status'] == 'FINISHED':
+        return True
     
-    # start the challenge
-    def start_challenge(self):
-        # get our token
-        form_data = {'uid': '904907409'}
-        self.token_param = requests.post(url + '/session', data=form_data).json()
+    print("Completed {levels_completed}/{total_levels} levels. " \
+        "New level: size {maze_size}, starting at {current_location}.".format(**game))
 
-        print("Obtained token: {}".format(self.token_param['token']))
+    return False
 
-        # begin playing games
-        is_finished = False
-        while True:
-            is_finished = self.get_game_info()
-            if is_finished:
-                print("Completed all levels. Challenge complete!")
-                return
-
-            # set up maze matrix
-            maze = [[False for x in range(self.game['maze_size'][0])] for y in range(self.game['maze_size'][1])]
-            self.play_level(maze, self.game['current_location'][0], self.game['current_location'][1])
-
-            print("Level complete!")
-
-    # returns True if challenge is finished, False if not
-    # we only need to check this at the start of each new level
-    def get_game_info(self):
-        self.game = requests.get(url + '/game', params=self.token_param).json()
-
-        # check status
-        if self.game['status'] == 'NONE':
-            print("Status is NONE, i.e. session has expired or does not exist")
-            sys.exit(1)
-        elif self.game['status'] == 'GAME_OVER':
-            print("Status is GAME_OVER, i.e. we lost :(")
-            sys.exit(1)
-        elif self.game['status'] == 'FINISHED':
-            return True
-        
-        print("Completed {levels_completed}/{total_levels} levels. " \
-            "New level: size {maze_size}, starting at {current_location}.".format(**self.game))
-
-        return False
-
-    # our matrix will be: False for unvisited, True for visited (or wall)
-    # note: format will be maze[y][x] because that's the internal format
-    def play_level(self, maze, x, y):
-        # print("({0}, {1}): {2}".format(x, y, maze[y][x]))
-        # self.get_game_info() # wastes a lot of time on needless network calls: for debugging only
-        
-        # try to move in every direction
-        for direction, offset in direcs.items():
-            newX = x + offset[0]
-            newY = y + offset[1]
-            # only move to new loc if it's valid
-            if self.is_valid(maze, newX, newY):
-                result = self.move(direction)
-                # we shouldn't have an OUT_OF_BOUNDS case here
-                # but even if we do, it keeps us in the same location so we should be good
-                if result == 'END':
+# our matrix will be: False for unvisited, True for visited (or wall)
+# note: format will be maze[y][x] because that's the internal format
+def play_level(maze, x, y):
+    # print("({0}, {1}): {2}".format(x, y, maze[y][x]))
+    # update_game_info() # wastes a lot of time on needless network calls: for debugging only
+    
+    # try to move in every direction
+    for direction, offset in direcs.items():
+        newX = x + offset[0]
+        newY = y + offset[1]
+        # only move to new loc if it's valid
+        if is_valid(maze, newX, newY):
+            result = move(direction)
+            # we shouldn't have an OUT_OF_BOUNDS case here
+            # but even if we do, it keeps us in the same location so we should be good
+            if result == 'END':
+                return True
+            elif result == 'WALL':
+                maze[newY][newX] = True # a wall is marked as True as well!
+            elif result == 'SUCCESS':
+                maze[newY][newX] = True
+                # try and keep going; if we returned True at any point, we already solved the maze!
+                if play_level(maze, newX, newY):
                     return True
-                elif result == 'WALL':
-                    maze[newY][newX] = True # a wall is marked as True as well!
-                elif result == 'SUCCESS':
-                    maze[newY][newX] = True
-                    # try and keep going; if we returned True at any point, we already solved the maze!
-                    if self.play_level(maze, newX, newY):
-                        return True
-                    # otherwise, we backtrack
-                    self.move(backtrack[direction])
-            
-        # visited locations return False!
-        return False
+                # otherwise, we backtrack
+                move(backtrack[direction])
+        
+    # we haven't solved the maze after checking every direc, so return false
+    return False
 
-    # returns True if current loc is valid (in bounds and unvisited)
-    def is_valid(self, maze, x, y):
-        return x >= 0 and x < self.game['maze_size'][0] and y >= 0 and y < self.game['maze_size'][1] and not maze[y][x]
+# returns True if current loc is valid (in bounds and unvisited)
+def is_valid(maze, x, y):
+    return x >= 0 and x < game['maze_size'][0] and y >= 0 and y < game['maze_size'][1] and not maze[y][x]
 
-    # moves us and returns the result of our move
-    def move(self, direction):
-        form_data = {'action': direction}
-        res = requests.post(url + '/game', data=form_data, params=self.token_param).json()
-        return res['result']
+# moves us and returns the result of our move
+def move(direction):
+    form_data = {'action': direction}
+    res = requests.post(url + '/game', data=form_data, params=token_param).json()
+    return res['result']
 
 # start the challenge :)
 if __name__ == "__main__":
-    c = Challenge()
-    c.start_challenge()
+    start_challenge()
